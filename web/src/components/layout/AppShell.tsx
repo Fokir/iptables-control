2850dbf7bedfc780ead3afa0d1e255cec2eddfd3
Andrew <sokol.fokir@gmail.com>
2026-03-11 01:49:59 +0400

    style(ui): ограничить максимальную ширину контента до 900px

--- web/src/components/layout/AppShell.tsx
+++ web/src/components/layout/AppShell.tsx
@@ -6,7 +6,9 @@ export function AppShell() {
     <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex-1 p-6 overflow-auto">
-        <Outlet />
+        <div className="mx-auto w-full max-w-[900px]">
+          <Outlet />
+        </div>
       </main>
     </div>
   )
